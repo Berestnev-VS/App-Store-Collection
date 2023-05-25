0768f6264758adf9b068e1c83392a2fba3cd2305
Berestnev Vladimir <berestnev.sv@gmail.com>
2023-05-25 11:13:17 +0300

    Initial Commit

---
+++ App Store Collection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App Store Collection
+//
+//  Created by Владимир on 25.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
